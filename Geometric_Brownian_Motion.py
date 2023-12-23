81764b63c0944e5d9e810af0f480b30f842ae261
ohmjariwala <145733466+ohmjariwala@users.noreply.github.com>
2023-12-22 20:34:24 -0500

    Update Geometric_Brownian_Motion.py

--- Geometric_Brownian_Motion.py
+++ Geometric_Brownian_Motion.py
@@ -12,7 +12,7 @@ class Geometric_Brownian_Motion:
     stock_ticker= input("Stock Ticker?")
     market_index_ticker = '^GSPC'  # S&P 500 index ticker
     
-    #Download data for the stock and market
+    # Download data for the stock and market
     stock_data = yf.download(stock_ticker, start_date, end_date)['Adj Close']
     market_data = yf.download(market_index_ticker, start_date, end_date)['Adj Close']
     
@@ -20,7 +20,7 @@ class Geometric_Brownian_Motion:
     stock_returns = stock_data.pct_change().dropna()
     market_returns = market_data.pct_change().dropna()
 
-    #Organize Returns data into pandas dataframe
+    # Organize Returns data into pandas dataframe
     data = pd.DataFrame({'Stock_Returns': stock_returns, 'Market_Returns': market_returns})
     data = data.dropna()
 
@@ -56,52 +56,54 @@ class Geometric_Brownian_Motion:
     simulations = 1000
     # initial stock price -> So
     So = stock_data.iloc[len(stock_data)-1].round(4)
-    #Volatility -> Sigma
+    # Volatility -> Sigma
     sigma = round(sqrt(covariance/ beta) *sqrt(21) , 5)
     
 
-    #Calculate each time step
+    # Calculate each time step
     dt = t/n
 
-    #Simulation using numpy arrays
+    # Simulation using numpy arrays
     St = np.exp( (mu - sigma ** 2 / 2) * dt + sigma * np.random.normal(0, np.sqrt(dt), size=(simulations,n)).T)
 
-    #Include array of 1's
+    # Include array of 1's
     St = np.vstack([np.ones(simulations), St])
 
-    #Multiply through by initial stock price and return the cumulative product of elements along a given simulation path (axis=0). 
+    # Multiply through by initial stock price and return the cumulative product of elements along a given simulation path. 
     St = So * St.cumprod(axis=0)
 
-    #time interval
+    # Time interval
     time = np.linspace(0,t,n+1)
 
     # Require numpy array that is the same shape as St
     tt = np.full(shape=(simulations,n+1), fill_value=time).T
 
+    
     # Generate a standard normal random variable
     Z = np.random.normal(0, 1, simulations)
 
-
-    #find end values of each simulation and add to an array
+    # Find the end values of each simulation and add to an array
     expected_stock_price_array=[]
     for i in range(simulations):
         final_value = St[:, i][-1]  # Extract the final value for the i-th simulation
         expected_stock_price_array.append(final_value) 
     
-    #Calculate expected stock price by taking the mean of the ending prices of all simulations
-    
-    
-    #expected_stock_price = So * np.exp((mu * t) + (sigma * Z * np.sqrt(t)))
+    # Calculate expected stock price by taking the mean of the ending prices of all simulations
     expected_stock_price= np.mean(expected_stock_price_array)
-    max_stock_value = np.max(St[-1])  # Maximum value of the stock over the entire simulation
+    
+    # Find the maximum value of the stock at the end of the simulation
+    max_stock_value = np.max(St[-1])
+    
+    # Find the minimum value of the stock at the end of the simulation
     min_stock_value= np.min(St[-1])
 
 
-    # Plot
+    # Print Statements
     print(f"Expected stock price at time t={t*12} months: {expected_stock_price:.2f}")
     print(f"Maximum stock value at time t={t*12} months: {max_stock_value:.2f}")
     print(f"Minimum stock value at time t={t*12} months: {min_stock_value:.2f}")
 
+    # Plot
     plt.plot(tt, St)
     plt.xlabel("Years $(t)$")
     plt.ylabel("Stock Price $(S_t)$")
